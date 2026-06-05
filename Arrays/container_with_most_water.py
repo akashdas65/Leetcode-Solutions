15d02118c5847df5f588cf79a5b9d537705dd324
Akash Das <akashdas2024@gift.edu.in>
2026-06-05 20:45:41 +0530

    Solved Leetcode 11 - cointainer with most water

---
+++ Arrays/container_with_most_water.py
@@ -0,0 +1,21 @@
+# LeetCode 11 - Container With Most Water
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            width = right - left
+            h = min(height[left], height[right])
+            max_area = max(max_area, width * h)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
