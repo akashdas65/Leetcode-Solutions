f5b811feb5cb25feaafe87f52ea0f68774ac15cb
Akash Das <akashdas2024@gift.edu.in>
2026-07-15 23:10:38 +0530

    Add removeelements method to solution class
    Implemented a method to remove specified elements from a list.

--- Arrays/remove_element.py
+++ Arrays/remove_element.py
@@ -1,1 +1,10 @@
-
+class solution:    
+    def removeelements(self,nums,val):
+        k=0
+        for i in range(len(nums)):
+            if nums[k] != val:
+                nums[i]=nums[k]
+                k+=1
+        return k
+                
+    
