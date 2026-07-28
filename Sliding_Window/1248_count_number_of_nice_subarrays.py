25140f35902a9e2770cdef3a1febfb352d306fc0
Akash Das <akashdas2024@gift.edu.in>
2026-07-28 22:43:51 +0530

    Create 1248_count_number_of_nice_subarrays.py

---
+++ Sliding_Window/1248_count_number_of_nice_subarrays.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        
+        def atMost(k):
+            left = 0
+            count = 0
+            ans = 0
+            
+            for right in range(len(nums)):
+                if nums[right] % 2 == 1:
+                    count += 1
+                
+                while count > k:
+                    if nums[left] % 2 == 1:
+                        count -= 1
+                    left += 1
+                
+                ans += right - left + 1
+            
+            return ans
+        
+        return atMost(k) - atMost(k-1)
